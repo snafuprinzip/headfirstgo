da2585c013759a454bd4c694d5782ac3d88d1902
Michael Leimenmeier <mleimenmeier@me.com>
2020-11-15 09:39:59 +0100

    package cleanup specific GOPATH set in make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+GOPATH=$HOME/go/src/github.com/snafuprinzip/headfirstgo/
 
-for srcfile in src/*.go; do 
+for srcfile in src/*/*.go; do 
 	/bin/echo -n "Building $srcfile ->"
 	go build -o bin $srcfile
 	echo " bin/"$(basename $srcfile | cut -d. -f1) 
--- src/tocelsius/tocelsius.go
+++ src/tocelsius/tocelsius.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/snafuprinzip/headfirstgo/keyboard"
+	"keyboard"
 )
 
 func main() {
